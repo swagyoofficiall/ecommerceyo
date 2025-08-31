dee46b956f55653a1bf23fe80a9842cc13cb6fc5
swagyoofficiall <swagyoofficial@gmail.com>
2025-08-31 20:00:43 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -34,12 +34,10 @@ const Navbar = () => {
 
       <div className="flex items-center gap-6">
         <img
-          onClick={() => {
-            setShowSearch(true);
-          }}
+          onClick={() => setShowSearch(true)}
           src={assets.search_icon}
           alt=""
-          className="w-5 cursor-pointer "
+          className="w-5 cursor-pointer"
         />
         <div className="group relative">
           <Link to="/login">
@@ -49,12 +47,10 @@ const Navbar = () => {
               className="w-5 cursor-pointer"
             />
           </Link>
-          
-          </div>
         </div>
 
-        <Link to="/cart" className="relative ">
-          <img src={assets.cart_icon} alt="" className="w-5 min-w-5 " />
+        <Link to="/cart" className="relative">
+          <img src={assets.cart_icon} alt="" className="w-5 min-w-5" />
           <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
             {getCartCount()}
           </p>
@@ -63,66 +59,36 @@ const Navbar = () => {
         <img
           src={assets.menu_icon}
           alt=""
-          className="w-5 cursor-pointer sm:hidden "
-          onClick={() => setVisible(!visible)} //visibility toggle (true/false))
+          className="w-5 cursor-pointer sm:hidden"
+          onClick={() => setVisible(!visible)}
         />
       </div>
 
       {/* Sidebar menu for small screens */}
       <div
         className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white ease-in duration-300
-             ${visible ? 'w-full' : 'w-0'}`}
+          ${visible ? 'w-full' : 'w-0'}`}
       >
-        <div className="flex flex-col text-gray-600 ">
+        <div className="flex flex-col text-gray-600">
           <div
-            onClick={() => {
-              setVisible(false);
-            }}
+            onClick={() => setVisible(false)}
             className="flex items-center gap-4 p-3 cursor-pointer"
           >
             <img src={assets.dropdown_icon} alt="" className="h-4 rotate-180" />
             <p className="font-semibold">Back</p>
           </div>
 
-          <NavLink
-            onClick={() => {
-              setVisible(false);
-            }}
-            className="py-2 pl-6 border"
-            to="/"
-          >
-            {' '}
-            HOME{' '}
+          <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border" to="/">
+            HOME
           </NavLink>
-          <NavLink
-            onClick={() => {
-              setVisible(false);
-            }}
-            className="py-2 pl-6 border"
-            to="/collection"
-          >
-            {' '}
-            COLLECTION{' '}
+          <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border" to="/collection">
+            COLLECTION
           </NavLink>
-          <NavLink
-            onClick={() => {
-              setVisible(false);
-            }}
-            className="py-2 pl-6 border"
-            to="/about"
-          >
-            {' '}
-            ABOUT{' '}
+          <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border" to="/about">
+            ABOUT
           </NavLink>
-          <NavLink
-            onClick={() => {
-              setVisible(false);
-            }}
-            className="py-2 pl-6 border"
-            to="/contact"
-          >
-            {' '}
-            CONTACT{' '}
+          <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border" to="/contact">
+            CONTACT
           </NavLink>
         </div>
       </div>
